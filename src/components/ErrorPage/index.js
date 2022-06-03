407c886318dfe6c0b87a2f31287ebeb2d312a4c4
karim216 <ahmedkarim.njiawouogbetnkom@esprit.tn>
2022-06-03 21:59:42 +0300

    new fill

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -12,6 +12,7 @@ const ErrorPage = () => {
         <div className="container">
           <h2 style={centerH2}>Oups cette page n'existe pas!!!</h2>
           <img src={batman} alt="Error page" />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora saepe quia voluptate magni ducimus quis nesciunt quibusdam, modi odio expedita voluptatum eaque reiciendis voluptatem porro, dolor in quaerat obcaecati tenetur. Rem dolor velit accusamus explicabo tempore iste, alias provident nisi unde commodi numquam cumque eveniet recusandae sint distinctio exercitationem consequuntur blanditiis facere delectus. Ad voluptatibus error rerum minus corrupti blanditiis beatae laudantium neque commodi, aliquid natus tempore nesciunt id, hic vero praesentium impedit repellendus quae perferendis a iste, sapiente eos qui dignissimos. Asperiores sed voluptatibus exercitationem harum! Asperiores molestias possimus placeat, reiciendis praesentium optio numquam, fuga, atque modi dolor iusto.</p>
         </div>
     </div>
   )
